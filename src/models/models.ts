7264f4cfc890d586e14a19557fd912f3f0887f05
Luka Saldivia <saldivialuka@gmail.com>
2024-12-24 17:19:38 -0300

    boselli models

--- src/models/models.ts
+++ src/models/models.ts
@@ -22,8 +22,19 @@ MODELS.CLIENTE.insertJoins()
 MODELS.PEDIDO.insertJoins()
 .join(TABLES.CLIENTE, [["cliente", "id"]])
 
+MODELS.PRODUCTO.insertJoins()
+.join(TABLES.GRUPO_PRODUCTO, [["grupo", "nombre"]])
+.join(TABLES.SECCION_PRODUCTO, [["seccion", "nombre"]])
+
 MODELS.RENGLON_PEDIDO.insertJoins()
 .join(TABLES.PEDIDO, [["id_pedido", "id"]])
 .join(TABLES.PRODUCTO, [["id_producto", "id"]])
+
+MODELS.ROL_X_PERMISO.insertJoins()
+.join(TABLES.ROL, [["id_rol", "nombre"]])
+.join(TABLES.PERMISO, [["id_permiso", "nombre"]])
+
+MODELS.USUARIO.insertJoins()
+.join(TABLES.ROL, [["rol", "nombre"]])
 
 export default MODELS
