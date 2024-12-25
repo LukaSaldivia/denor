8ff29939fda738278ea1dcd0eb4dba2ab5bd9c62
Luka Saldivia <saldivialuka@gmail.com>
2024-12-24 21:46:00 -0300

    boselli

--- src/routes/home.routes.ts
+++ src/routes/home.routes.ts
@@ -5,46 +5,19 @@ const router = Router()
 
 router.get('/', async(req, res) => {
 
-//   MODELS.CLIENTE.prepareSearch()
-// .appendFilter({
-//   field : "vendedor",
-//   type : "text",
-//   value : "",
-//   score : 9
-  
-// })
-
-
-// let [results] = await MODELS.CLIENTE.search(0, {
-//   sortBy : [
-//     {
-//       field : 'relevance',
-//       order : 'DESC'
-//     }
-//   ]
-// })
-// res.send(results)
-
-// MODELS.ROL.prepareSearch()
-// .appendFilter({
-//   field : 'nombre',
-//   type : 'text',
-//   value : 'admIni'
-// })
-
-
-// let [results] = await MODELS.ROL.search()
-
-// res.send(results)
-
 MODELS.PRODUCTO.prepareSearch()
 .appendFilter({
   field : "grupo",
   type : "text",
   value : "Aliger"
 })
+.appendFilter({
+  field : 'nombre',
+  type : 'text',
+  value : "arra"
+})
 
-let results = await MODELS.ROL.search()
+let results = await MODELS.PRODUCTO.search()
 
 res.send(results)
 
